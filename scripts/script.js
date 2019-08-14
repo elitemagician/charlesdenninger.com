d7fb1e98d64597dc2c29aca58d61c00a7160d6b8
elitemagician <isles4life+1@gmail.com>
2019-08-14 15:49:49 -0400

    update to resume

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,3 @@
-// alert("Hello World");
-
 $(document).ready(function() {
  $(".pdf").click(function() {
   $.fancybox({
@@ -14,3 +12,14 @@ $(document).ready(function() {
   return false;
  }); // pdf 
 }); // ready
+
+$(document).ready(function() {
+/* Apply fancybox to multiple items */
+
+$("a.iframe").fancybox({
+'width': 640, // or whatever you want
+'height': 480, // or whatever you want
+'type': 'iframe'
+});
+
+});
