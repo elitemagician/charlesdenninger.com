cf291c0e3d89d5780902538d2d2a771a37cdccb0
elitemagician <isles4life+1@gmail.com>
2019-08-20 23:07:11 -0400

    phpmailer skeleton

---
+++ contact-form-handler.php
@@ -0,0 +1,50 @@
+<?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+$email = $_POST['email'];
+$reasons = $_POST['reasons'];
+$comments = $_POST['comments'];
+
+// Load Composer's autoloader
+require 'vendor/autoload.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                                       // Enable verbose debug output
+    $mail->isSMTP();                                            // Set mailer to use SMTP
+    $mail->Host       = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'assistlylite@gmail.com';                     // SMTP username
+    $mail->Password   = 'Salesforce1';                               // SMTP password
+    $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
+    $mail->Port       = 587;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom($email);
+    $mail->addAddress('cable82guy@gmail.com');     // Add a recipient
+    // $mail->addAddress('ellen@example.com');               // Name is optional
+    // $mail->addReplyTo('info@example.com', 'Information');
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
+
+    // Attachments
+    $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'You have a professional inquiry from charlesdenninger.com';
+    $mail->Body    = $comments;
+    $mail->AltBody = $comments;
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Do not pass go, Do not collect $200 dollars! Mailer Error: {$mail->ErrorInfo}";
+}
